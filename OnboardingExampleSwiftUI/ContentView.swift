02edf557d9e852058e982476329221851eadb58f
DaneThomas <dane77thomas@gmail.com>
2020-10-16 14:36:41 -0500

    created a basic layout in swiftui

--- OnboardingExampleSwiftUI/ContentView.swift
+++ OnboardingExampleSwiftUI/ContentView.swift
@@ -8,12 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     @ObservedObject var viewModel: OnboardingViewModel
+
     var body: some View {
-        Image(viewModel.onboardingModel.backgroundImageName)
-            .resizable()
-            .ignoresSafeArea()
+        ZStack{
+            VStack {
+                Spacer()
+                ZStack {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Image(systemName: "location.fill")
+                            Text(viewModel.onboardingModel.title)
+                            Text("more text")
+                            Text("second line of text that is much longer and longer and longer")
+                            Button("button title") {
+                                print("button clicked")
+                            }
+                        }
+                        Spacer()
+                    }
+                    .foregroundColor(.white)
+                }
+                .padding()
+                .background(Color.black.opacity(0.8))
+                .cornerRadius(10)
+            }
+            .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
+        }
+        .background(Image(viewModel.onboardingModel.backgroundImageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill).ignoresSafeArea())
     }
 }
 
