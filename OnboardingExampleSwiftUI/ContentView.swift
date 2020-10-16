5ac819d7e0ce06286dd5938c3790b708fb539faa
DaneThomas <dane77thomas@gmail.com>
2020-10-16 12:26:42 -0500

    initial commit

---
+++ OnboardingExampleSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnboardingExampleSwiftUI
+//
+//  Created by Dane Thomas on 10/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnboardingExampleSwiftUI/OnboardingExampleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingExampleSwiftUIApp.swift
+//  OnboardingExampleSwiftUI
+//
+//  Created by Dane Thomas on 10/16/20.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingExampleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
